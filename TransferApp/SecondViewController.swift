5cabeeb08e08f2fb6acf6341614211bd78758a7d
studyingProjects <148430835+studyingProjects@users.noreply.github.com>
2024-02-14 15:03:53 +0300

    create initial UI

---
+++ TransferApp/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  TransferApp
+//
+//  Created by Andrei Shpartou on 14/02/2024.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet var dataTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TransferApp/ViewController.swift
+++ TransferApp/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var dataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
